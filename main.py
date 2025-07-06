2c7ba6279975c235ed5ec16369ad3c26e032d20c
Christian <kalonjic34@gmail.com>
2025-07-06 13:45:27 +0200

    Enhance PDF by adding consistent horizontal lines across pages

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ for index, row in df.iterrows():
     pdf.cell(w=0,h=12, txt=row["Topic"], align="L", 
          ln=1,)
     pdf.line(10,21, 200,21)
+    for i in range(20,298,10):
+        pdf.line(10,i,200,i)
+    pdf.line(10,21,200,21)
     
     # set footer
     pdf.ln(265)
@@ -30,5 +33,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=8,h=10,txt=row["Topic"], align="R")
+        
+        for y in range(20,298,10):
+            pdf.line(10,y,200,y)
+
 
 pdf.output("output.pdf")
